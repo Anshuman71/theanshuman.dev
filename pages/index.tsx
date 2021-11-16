01052a37ceebc5966f8769bb4a2e4a72577f6305
Anshuman Bhardwaj <anshu5074@gmail.com>
2021-11-16 04:20:00 -0800

    fix apos

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main className={'w-2/3 mx-auto'}>
-                <h1 className={'text-gray-100 text-5xl'}>Hi, I'm Anshuman</h1>
+                <h1 className={'text-gray-100 text-5xl'}>Hi, I&apos;m Anshuman</h1>
                 <p className={'text-gray-300 text-lg mt-10 w-2/3'}> I love sammy🐶, music (Bollywood & Enrique) and gaming (MOHW and DMC). <br/> Like Batman 🦇, I work late
                     at night. I like teaching and sharing what I learned mostly the hard way. Other than that I build
                     tools for humans 😎.
