5bc64877d8f801d71d9c0bce109674b1966b1375
Anshuman Bhardwaj <anshu5074@gmail.com>
2021-11-18 00:04:44 -0800

    - extract Head into MetaData

---
+++ components/MetaData.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+export default function MetaData() {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta
+        name="description"
+        content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
+      />
+      <meta
+        name="keywords"
+        content="anshuman, anshuman5221, anshuman71, anshuman bhardwaj, Anshuman, Anshuman Bhardwaj Canoo, Anshuman Bhardwaj Delightree, Anshuman Bhardwaj Collegebasket, Anshuman Bhardwaj Keshav Mahavidyalaya, Anshuman Bhardwaj Delhi University, DU, KMV, Delhi, Gurgaon, India, Canoo"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#000" />
+      <meta property="og:title" content="Anshuman Bhardwaj" />
+      <meta property="og:type" content="website" />
+      <meta name="author" content="Anshuman Bhardwaj" />
+      <meta property="og:image" content="https://anshuman.vercel.app" />
+      <meta property="og:image:height" content="800" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="480" />
+      <meta property="og:url" content="https://anshuman.vercel.app" />
+      <meta
+        property="og:description"
+        content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
+      />
+      <meta property="og:site_name" content="Anshuman Bhardwaj" />
+      <meta name="robots" content="index, follow" />
+      <title>Anshuman Bhardwaj</title>
+      <link rel="icon" sizes="96x96" type="image/png" href="/icon.png" />
+    </Head>
+  );
+}
