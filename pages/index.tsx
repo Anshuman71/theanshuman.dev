d35ffe1c5ab3ce517bc29463e59379d5ef1990f4
Anshuman Bhardwaj <anshu5074@gmail.com>
2021-11-18 00:14:52 -0800

    - add framer-motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,64 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import { contacts, experiences } from "../constants";
 import ExternalLink from "../components/ExternalLink";
 import HitCounter from "../components/HitCounter";
+import { motion } from "framer-motion";
+import MetaData from "../components/MetaData";
+
+const containerVariants = {
+  hidden: { background: "rgba(0,0,0,0.8)" },
+  enter: { background: "rgb(0,0,0)" },
+};
+
+const mainVariants = {
+  initial: {
+    y: -10,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const sectionVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+};
 
 const Home: NextPage = () => {
   return (
-    <div className={"bg-black p-10 pt-20 min-h-screen"}>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta
-          name="description"
-          content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
-        />
-        <meta
-          name="keywords"
-          content="anshuman, anshuman5221, anshuman71, anshuman bhardwaj, Anshuman, Anshuman Bhardwaj Canoo, Anshuman Bhardwaj Delightree, Anshuman Bhardwaj Collegebasket, Anshuman Bhardwaj Keshav Mahavidyalaya, Anshuman Bhardwaj Delhi University, DU, KMV, Delhi, Gurgaon, India, Canoo"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content="#000" />
-        <meta property="og:title" content="Anshuman Bhardwaj" />
-        <meta property="og:type" content="website" />
-        <meta name="author" content="Anshuman Bhardwaj" />
-        <meta property="og:image" content="https://anshuman.vercel.app" />
-        <meta property="og:image:height" content="800" />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="480" />
-        <meta property="og:url" content="https://anshuman.vercel.app" />
-        <meta
-          property="og:description"
-          content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
-        />
-        <meta property="og:site_name" content="Anshuman Bhardwaj" />
-        <meta name="robots" content="index, follow" />
-        <title>Anshuman Bhardwaj</title>
-        <link rel="icon" sizes="96x96" type="image/png" href="/icon.png" />
-      </Head>
-      <main className={"w-2/3 mx-auto"}>
-        <section className="intro w-2/3">
+    <motion.div
+      variants={containerVariants}
+      initial={"hidden"}
+      animate="enter"
+      transition={{
+        duration: 0.5,
+      }}
+      className={"bg-black p-10 pt-20 min-h-screen"}
+    >
+      <MetaData />
+      <motion.main
+        variants={mainVariants}
+        initial={"initial"}
+        animate={"animate"}
+        className={"w-3/4 mx-auto"}
+      >
+        <motion.section
+          variants={sectionVariants}
+          animate={"animate"}
+          initial={"initial"}
+          className="intro w-2/3"
+        >
           <h1 className={"text-gray-100 text-5xl"}>Hi, I&apos;m Anshuman</h1>
           <p className={"text-gray-300 text-lg mt-10"}>
             {" "}
@@ -46,8 +67,13 @@ const Home: NextPage = () => {
             and sharing what I learned mostly the hard way. Other than that I
             build tools for humans 😎.
           </p>
-        </section>
-        <section className="what-i-do w-2/3 text-gray-200 my-10">
+        </motion.section>
+        <motion.section
+          variants={sectionVariants}
+          animate={"animate"}
+          initial={"initial"}
+          className="what-i-do w-2/3 text-gray-200 my-10"
+        >
           <h2 className="text-3xl mb-1 tracking-wider">What I do?</h2>
           <p className="text-gray-300 text-lg mt-4">
             I am a Full Stack Developer. I have been working with ReactJS and
@@ -57,9 +83,13 @@ const Home: NextPage = () => {
             <b className="font-medium">serverless.</b> <br />I love cloud
             functions and automating workflow using them.
           </p>
-          <div className="flex py-5 flex-row flex-wrap justify-around items-center"></div>
-        </section>
-        <section className={"what-so-far text-gray-200 my-10 w-2/3"}>
+        </motion.section>
+        <motion.section
+          variants={sectionVariants}
+          animate={"animate"}
+          initial={"initial"}
+          className={"what-so-far text-gray-200 my-10 w-2/3"}
+        >
           <h2 className="text-3xl mb-1 tracking-wider">What so far?</h2>
           <div className={"my-4"}>
             {experiences.map((exp) => (
@@ -72,15 +102,20 @@ const Home: NextPage = () => {
           {contacts.map((contact) => (
             <ExternalLink key={contact.url} {...contact} />
           ))}
-        </section>
-        <footer className={"text-gray-300"}>
+        </motion.section>
+        <motion.footer
+          variants={sectionVariants}
+          animate={"animate"}
+          initial={"initial"}
+          className={"text-gray-300"}
+        >
           <HitCounter />
           <p className="p-2 text-center tracking-wide">
             Made with ♥ and tailwind.css by Anshuman Bhardwaj
           </p>
-        </footer>
-      </main>
-    </div>
+        </motion.footer>
+      </motion.main>
+    </motion.div>
   );
 };
 
