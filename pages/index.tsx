357c09193b0abeb776647765f44f6a0eb40bec94
Anshuman Bhardwaj <anshu5074@gmail.com>
2021-11-22 02:51:53 -0800

    - add animations to tech stack

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import type { NextPage } from "next";
 import { contacts, experiences, technologies } from "../constants";
 import ExternalLink from "../components/ExternalLink";
 import HitCounter from "../components/HitCounter";
-import { motion } from "framer-motion";
+import { motion, TargetAndTransition, VariantLabels } from "framer-motion";
 import MetaData from "../components/MetaData";
+import SectionHeading from "../components/SectionHeading";
+import Section from "../components/Section";
 
 const containerVariants = {
   hidden: { background: "rgba(0,0,0,0.8)" },
@@ -20,6 +22,31 @@ const mainVariants = {
       delay: 0.7,
       duration: 1,
     },
+  },
+};
+
+const techVariants = {
+  initial: {
+    y: 20,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+const hover: { [key: string]: VariantLabels | TargetAndTransition } = {
+  react: {
+    rotate: 360,
+    transition: {
+      ease: "linear",
+      duration: 4,
+      repeat: Infinity,
+    },
+  },
+  others: {
+    scale: 1.05,
   },
 };
 
@@ -41,19 +68,19 @@ const Home: NextPage = () => {
         animate={"animate"}
         className={"w-3/4 mx-auto"}
       >
-        <motion.section className="intro w-2/3">
-          <h1 className={"text-gray-100 text-5xl"}>Hi, I&apos;m Anshuman</h1>
-          <p className={"text-gray-300 text-lg mt-10 leading-loose"}>
+        <Section>
+          <h1 className={"text-gray-100 text-6xl"}>Hi, I&apos;m Anshuman</h1>
+          <p className={"text-gray-300 text-lg mt-4 leading-loose"}>
             {" "}
             I love sammy🐶, music (Bollywood & Enrique) and gaming (MOHW and
             DMC). <br /> Like Batman 🦇, I work late at night. I like teaching
             and sharing what I learned mostly the hard way. Other than that I
             build tools for humans 😎.
           </p>
-        </motion.section>
-        <motion.section className="what-i-do w-2/3 text-gray-200 my-10">
-          <h2 className="text-3xl mb-1 tracking-wider">What I do?</h2>
-          <p className="text-gray-300 text-lg mt-4 leading-loose">
+        </Section>
+        <Section>
+          <SectionHeading>What I do?</SectionHeading>
+          <p className="text-gray-300 text-lg leading-loose">
             I am a Full Stack Developer. I have been working with ReactJS and
             NodeJS since 2017. On my way I have built many apps utilizing their
             core and many on-the-top libraries. I love working with React and
@@ -61,53 +88,59 @@ const Home: NextPage = () => {
             <b className="font-medium">serverless.</b> <br />I love cloud
             functions and automating workflow using them.
           </p>
-        </motion.section>{" "}
-        <motion.section className="technologies text-gray-200 my-10">
-          <h2 className="text-3xl mb-2 tracking-wider">
-            Favourite technologies
-          </h2>
-          <div className={"flex flex-row flex-wrap"}>
+        </Section>{" "}
+        <Section>
+          <SectionHeading>Favourite technologies</SectionHeading>
+          <motion.div
+            animate={"animate"}
+            initial={"initial"}
+            variants={{
+              initial: { opacity: 0 },
+              animate: {
+                opacity: 1,
+                transition: { staggerChildren: 0.2, delayChildren: 1 },
+              },
+            }}
+            className={"flex flex-row flex-wrap"}
+          >
             {technologies.map((tech) => (
-              <div key={tech.url}>
+              <motion.div
+                variants={techVariants}
+                whileHover={{
+                  scale: 1.025,
+                }}
+                key={tech.url}
+                className={"p-4 bg-[#303030] rounded-xl mr-12 my-4"}
+              >
                 <motion.img
-                  whileHover={
-                    tech.name === "react"
-                      ? {
-                          rotate: 360,
-                          transition: {
-                            ease: "linear",
-                            duration: 4,
-                            repeat: Infinity,
-                          },
-                        }
-                      : { scale: 1.1 }
-                  }
+                  whileHover={hover[tech.name] || hover.others}
                   alt="react"
-                  className={"my-4 mr-16 w-32"}
+                  className={"w-24"}
                   src={tech.url}
                 />
-              </div>
+              </motion.div>
             ))}
-          </div>
-        </motion.section>
-        <motion.section className={"what-so-far text-gray-200 my-10 w-2/3"}>
-          <h2 className="text-3xl mb-1 tracking-wider">What so far?</h2>
-          <div className={"my-4"}>
+          </motion.div>
+        </Section>
+        <Section>
+          <SectionHeading>What so far?</SectionHeading>
+          <div>
             {experiences.map((exp) => (
               <ExternalLink key={exp.url} {...exp} />
             ))}
-            <i className="font-light">...many more on github</i>
           </div>
-          <br />
-          <h2 className="text-3xl mt-10 mb-1">Where to find me?</h2>
+        </Section>
+        <Section>
+          <SectionHeading>Where to find me?</SectionHeading>
           {contacts.map((contact) => (
             <ExternalLink key={contact.url} {...contact} />
           ))}
-        </motion.section>
-        <motion.footer className={"text-gray-300"}>
+        </Section>
+        <motion.footer className={"text-gray-300 py-10"}>
           <HitCounter />
-          <p className="p-2 text-center tracking-wide">
-            Made with ♥ and tailwind.css by Anshuman Bhardwaj
+          <p className="p-2 text-lg text-center tracking-wide">
+            Made with <span className={"text-2xl text-red-600"}>♥</span> and
+            tailwindcss by <i>Anshuman Bhardwaj</i>
           </p>
         </motion.footer>
       </motion.main>
