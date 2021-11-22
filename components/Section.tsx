89aab805ee3b90004cc39e83de472de4fb342063
Anshuman Bhardwaj <anshu5074@gmail.com>
2021-11-22 02:50:31 -0800

    - create and use common components

---
+++ components/Section.tsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+import { PropsWithChildren } from "react";
+
+export default function Section({
+  children,
+  className,
+}: PropsWithChildren<any>) {
+  return (
+    <motion.section className={`text-gray-200 mb-16 ${className}`}>
+      {children}
+    </motion.section>
+  );
+}
---
+++ components/SectionHeading.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+
+export default function SectionHeading({
+  children,
+  className,
+}: PropsWithChildren<any>) {
+  return (
+    <h2 className={`text-4xl mb-4 tracking-wider ${className}`}>{children}</h2>
+  );
+}
