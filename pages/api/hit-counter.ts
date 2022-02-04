dc53b24ac84e235a5db5868d1071104733733c1b
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-02-04 15:44:18 +0530

    use mongodb instead of fauna

--- pages/api/hit-counter.ts
+++ pages/api/hit-counter.ts
@@ -1,35 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import faunadb from "faunadb";
-
-const secret = process.env.FAUNA_DB_SECRET || "<dummy_secret>";
-const domain = process.env.FAUNA_DB_DOMAIN || "db.eu.fauna.com";
-const collection = process.env.FAUNA_DB_COLLECTION || "<collection-name>";
-const docID = process.env.FAUNA_DB_DOC_ID || "<doc-ref-id>";
+import connectToDatabase from "../../mongodb";
 
 export default async function incrementHitCounter(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const { Add, Update, Select, query, Ref } = faunadb;
-    const client = new faunadb.Client({
-      secret,
-      domain,
-    });
+    const db = await connectToDatabase();
+    const collection = db.collection("hit-counter");
 
-    const docRef = Ref(faunadb.Collection(collection), docID);
-    const doc = query.Get(docRef);
-    // Fetch the document for-real
-    const document = await client.query(
-      Update(docRef, {
-        data: {
-          value: Add(Select(["data", "value"], doc), 1),
-        },
-      })
+    const document = await collection.findOneAndUpdate(
+      { id: "hit-counter" },
+      { $inc: { value: 1 } },
+      { returnDocument: "after" }
     );
     //@ts-ignore
-    return res.status(200).json({ value: document.data.value });
+    return res.status(200).json({ value: document.value.value });
   } catch (e) {
+    console.log("error", e);
     res.json({ error: "yes", value: "too hard to count..." });
   }
 }
