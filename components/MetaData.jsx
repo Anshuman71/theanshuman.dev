c30472468c3bc04ac5852bd1eebc5a12c2198d4c
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-02-04 17:54:03 +0530

    Update metadata

--- components/MetaData.jsx
+++ components/MetaData.jsx
@@ -6,10 +6,9 @@ export default function MetaData() {
       <meta charSet="UTF-8" />
       <title>Anshuman Bhardwaj</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/icon-96x96.png" />
       <meta
         name="description"
-        content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
+        content="Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
       />
       <meta
         name="keywords"
@@ -25,22 +24,22 @@ export default function MetaData() {
       />
       <meta property="og:site_name" content="Anshuman Bhardwaj" />
       <meta property="og:image" content="https://theanshuman.dev/me.jpeg" />
-      <meta property="og:image:height" content="400" />
       <meta property="og:image:type" content="image/jpeg" />
-      <meta property="og:image:width" content="480" />
+      <meta property="og:image:height" content="400" />
+      <meta property="og:image:width" content="400" />
       <meta property="og:url" content="https://theanshuman.dev" />
       <meta name="author" content="Anshuman Bhardwaj" />
 
-      <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://theanshuman.dev" />
+      <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:title" content="Anshuman Bhardwaj" />
-      <meta
-        property="twitter:description"
-        content="A project to automatically tweet your popular articles from DEV. Built by Anshuman."
-      />
       <meta
         property="twitter:image:src"
         content="https://theanshuman.dev/me.jpeg"
+      />
+      <meta
+        property="twitter:description"
+        content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business."
       />
 
       <meta name="robots" content="index, follow" />
