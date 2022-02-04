1af652ce98655cc3fb2aa92a893a1b22bf4e0db2
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-02-04 17:48:28 +0530

    Update MetaData

--- components/MetaData.jsx
+++ components/MetaData.jsx
@@ -4,32 +4,46 @@ export default function MetaData() {
   return (
     <Head>
       <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      {/* <!-- Primary Meta Tags --> */}
+      <title>Anshuman Bhardwaj</title>
+      <meta name="title" content="Anshuman Bhardwaj" />
       <meta
         name="description"
-        content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
+        content="Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
       />
+      <meta name="theme-color" content="#F1C40F" />
       <meta
         name="keywords"
-        content="anshuman, anshuman5221, anshuman71, anshuman bhardwaj, Anshuman, Anshuman Bhardwaj Canoo, Anshuman Bhardwaj Delightree, Anshuman Bhardwaj Collegebasket, Anshuman Bhardwaj Keshav Mahavidyalaya, Anshuman Bhardwaj Delhi University, DU, KMV, Delhi, Gurgaon, India, Canoo"
+        content="sun_anshuman, Anshuman71, anshuman_bhardwaj, anshuman-bhardwaj, Anshuman, Anshuman Bhardwaj Canoo, Anshuman Bhardwaj Delightree, Anshuman Bhardwaj Collegebasket, Anshuman Bhardwaj Keshav Mahavidyalaya, Anshuman Bhardwaj Delhi University, DU, KMV, Delhi, Gurgaon, India, Canoo"
       />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="theme-color" content="#000" />
-      <meta property="og:title" content="Anshuman Bhardwaj" />
+      {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
-      <meta name="author" content="Anshuman Bhardwaj" />
-      <meta property="og:image" content="https://theanshuman.dev" />
-      <meta property="og:image:height" content="800" />
-      <meta property="og:image:type" content="image/png" />
-      <meta property="og:image:width" content="480" />
-      <meta property="og:url" content="https://theanshuman.dev" />
+      <meta property="og:url" content="https://theanshuman.dev/" />
+      <meta property="og:title" content="Anshuman Bhardwaj" />
       <meta
         property="og:description"
-        content="Hey there, I'm Anshuman a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
+        content="Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
       />
-      <meta property="og:site_name" content="Anshuman Bhardwaj" />
-      <meta name="robots" content="index, follow" />
-      <title>Anshuman Bhardwaj</title>
-      <link rel="icon" sizes="96x96" type="image/png" href="/icon.png" />
+      <meta property="og:image" content="https://theanshuman.dev/me.jpeg" />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://theanshuman.dev/" />
+      <meta property="twitter:title" content="Anshuman Bhardwaj" />
+      <meta
+        property="twitter:description"
+        content="Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
+      />
+      <meta
+        property="twitter:image"
+        content="https://theanshuman.dev/me.jpeg"
+      />
+      <link
+        rel="icon"
+        type="image/jpeg"
+        href="https://theanshuman.dev/icon.jpeg"
+      />
     </Head>
   );
 }
