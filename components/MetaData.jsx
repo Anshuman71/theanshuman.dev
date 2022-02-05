1ff7b3ad782552cd35cf14a2e4ad113c12765865
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-02-05 21:05:29 +0530

    make metadata custom

--- components/MetaData.jsx
+++ components/MetaData.jsx
@@ -1,6 +1,16 @@
 import Head from "next/head";
 
-export default function MetaData({ title = "Anshuman Bhardwaj" }) {
+const DEFAULT_DESCRIPTION =
+  "Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. ";
+
+const DEFAULT_KEYWORDS =
+  "sun_anshuman, Anshuman71, anshuman_bhardwaj, anshuman-bhardwaj, Anshuman, Anshuman Bhardwaj Canoo,  Delightree,  Collegebasket, Keshav Mahavidyalaya, Delhi University, India, Canoo";
+
+export default function MetaData({
+  title = "Anshuman Bhardwaj",
+  description = DEFAULT_DESCRIPTION,
+  keywords = "",
+}) {
   return (
     <Head>
       <meta charSet="UTF-8" />
@@ -8,33 +18,21 @@ export default function MetaData({ title = "Anshuman Bhardwaj" }) {
       {/* <!-- Primary Meta Tags --> */}
       <title>{title}</title>
       <meta name="title" content={title} />
-      <meta
-        name="description"
-        content="Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
-      />
+      <meta name="description" content={description} />
       <meta name="theme-color" content="#F1C40F" />
-      <meta
-        name="keywords"
-        content="sun_anshuman, Anshuman71, anshuman_bhardwaj, anshuman-bhardwaj, Anshuman, Anshuman Bhardwaj Canoo, Anshuman Bhardwaj Delightree, Anshuman Bhardwaj Collegebasket, Anshuman Bhardwaj Keshav Mahavidyalaya, Anshuman Bhardwaj Delhi University, DU, KMV, Delhi, Gurgaon, India, Canoo"
-      />
+      <meta name="keywords" content={`${DEFAULT_KEYWORDS} , ${keywords}`} />
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://theanshuman.dev/" />
       <meta property="og:title" content={title} />
-      <meta
-        property="og:description"
-        content="Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
-      />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content="https://theanshuman.dev/me.jpeg" />
 
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://theanshuman.dev/" />
       <meta property="twitter:title" content={title} />
-      <meta
-        property="twitter:description"
-        content="Hey there, I'm Anshuman Bhardwaj, a self-taught developer passionate about empowering people with the skills I have learned. I love building an awesomely beautiful frontend for seamless user experience and flawless back end for a scalable & reliable business. "
-      />
+      <meta property="twitter:description" content={description} />
       <meta
         property="twitter:image"
         content="https://theanshuman.dev/me.jpeg"
