3acfef3070cc46d0341c74388e9b076ec6a8c96e
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-09-26 09:02:52 +0530

    Desc blog order

--- constants.ts
+++ constants.ts
@@ -207,6 +207,36 @@ function randomLikes() {
 
 export const externalArticles: ArticleInList[] = [
   {
+    canonical_url:
+      "https://blog.tigrisdata.com/http2-websockets-future-of-real-time",
+    slug: "http2-websockets-future-of-real-time",
+    title:
+      "HTTP/2, WebSockets, and the Future of Real-Time Client-Server Interactions",
+    description:
+      "Real-time client-server interactions will only become more vital as  applications and use cases continue to evolve. Though both the WebSocket and  HTTP/2 protocols can work well for real-time communication, HTTP/2 is the  better option in certain situations.",
+    cover_image: "https://imgur.com/qsOEaIn.png",
+    tag_list: ["tigris", "tigris data", "real-time", "http/2", "websockets"],
+    reading_time_minutes: 11,
+    published_at: "Wed Sep 26 2022",
+    page_views_count: randomPageViews(),
+    positive_reactions_count: randomLikes(),
+  },
+  {
+    canonical_url:
+      "https://blog.logrocket.com/using-convex-for-state-management/",
+    slug: "using-convex-for-state-management",
+    title: "A guide to using Convex for state management",
+    description:
+      "Managing an application's state has become easier, but the global state concept is still a pain for developers, which Convex aims to solve.",
+    cover_image:
+      "https://blog.logrocket.com/wp-content/uploads/2022/09/guide-using-convex-state-management-nocdn.png",
+    tag_list: ["convex", "state management", "nextjs", "global state"],
+    reading_time_minutes: 9,
+    published_at: "Wed Sep 21 2022",
+    page_views_count: randomPageViews(),
+    positive_reactions_count: randomLikes(),
+  },
+  {
     canonical_url: "https://www.tiny.cloud/blog/bootstrap-image-upload/",
     slug: "tiny-mce-bootstrap-image-upload",
     title: "How to get started with image uploads using Bootstrap",
@@ -232,36 +262,6 @@ export const externalArticles: ArticleInList[] = [
     tag_list: ["merge_api", "developer", "hris", "payroll"],
     reading_time_minutes: 5,
     published_at: "Wed Aug 10 2022",
-    page_views_count: randomPageViews(),
-    positive_reactions_count: randomLikes(),
-  },
-  {
-    canonical_url:
-      "https://blog.logrocket.com/using-convex-for-state-management/",
-    slug: "using-convex-for-state-management",
-    title: "A guide to using Convex for state management",
-    description:
-      "Managing an application's state has become easier, but the global state concept is still a pain for developers, which Convex aims to solve.",
-    cover_image:
-      "https://blog.logrocket.com/wp-content/uploads/2022/09/guide-using-convex-state-management-nocdn.png",
-    tag_list: ["convex", "state management", "nextjs", "global state"],
-    reading_time_minutes: 9,
-    published_at: "Wed Sep 21 2022",
-    page_views_count: randomPageViews(),
-    positive_reactions_count: randomLikes(),
-  },
-  {
-    canonical_url:
-      "https://blog.tigrisdata.com/http2-websockets-future-of-real-time",
-    slug: "http2-websockets-future-of-real-time",
-    title:
-      "HTTP/2, WebSockets, and the Future of Real-Time Client-Server Interactions",
-    description:
-      "Real-time client-server interactions will only become more vital as  applications and use cases continue to evolve. Though both the WebSocket and  HTTP/2 protocols can work well for real-time communication, HTTP/2 is the  better option in certain situations.",
-    cover_image: "https://imgur.com/qsOEaIn.png",
-    tag_list: ["tigris", "tigris data", "real-time", "http/2", "websockets"],
-    reading_time_minutes: 11,
-    published_at: "Wed Sep 26 2022",
     page_views_count: randomPageViews(),
     positive_reactions_count: randomLikes(),
   },
