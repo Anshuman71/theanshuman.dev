5b5138413671b73a6597a104d7d388ea08815b25
Anshuman Bhardwaj <anshu5074@gmail.com>
2023-03-04 20:37:34 +0530

    fix link styling

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -5,7 +5,7 @@ export default function ExternalLink({ url, name }: LinkInformationType) {
     <a
       target={"_blank"}
       rel="noreferrer"
-      className="text-gray-300 tracking-widest text-lg inline-block my-2 mr-10 pb-1 border-gray-400 border-solid border-b-2 hover:border-yellow-500 hover:text-yellow-500 transition-all duration-75"
+      className="text-gray-300 tracking-widest text-lg inline-block my-2 mr-10 pb-1 underline underline-offset-8 hover:text-yellow-500 transition-all duration-75"
       href={url}
     >
       {name}
