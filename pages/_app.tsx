cc62a3b3a157930b1d0b7d48e56cea7f96c29a47
Anshuman Bhardwaj <anshu5074@gmail.com>
2023-05-22 07:51:38 +0530

    add docsly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import NavBar from "../components/NavBar";
 import { MDXProvider } from "@mdx-js/react";
+import Docsly from "@docsly/react";
+import "@docsly/react/styles.css";
 
 const components = {
   code: (props: any) => {
@@ -14,12 +17,17 @@ const components = {
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter();
   return (
     <div className={"bg-zinc-900 flex flex-col min-h-screen text-gray-200"}>
       <NavBar />
       <MDXProvider components={components}>
         <Component {...pageProps} />
       </MDXProvider>
+      <Docsly
+        pathname={asPath}
+        publicId="public_aiLJByn9Q503I1PfzZ8YTvrW0YNuVnLc2J5pDq5EgoI4Z3RfUMNsJTa33YpDVbE6"
+      />
     </div>
   );
 }
