6957150dc53c5a644f581d885f5f303b34f201ef
Anshuman Bhardwaj <anshu5074@gmail.com>
2023-05-29 05:33:55 +0530

    update url to public domain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </MDXProvider>
       <Docsly
-        baseURL="https://public-api.sunshine23.workers.dev/api/v1.8"
+        baseURL="https://public.docsly.dev/api/v1.8"
         pathname={asPath}
         publicId="public_aiLJByn9Q503I1PfzZ8YTvrW0YNuVnLc2J5pDq5EgoI4Z3RfUMNsJTa33YpDVbE6"
       />
