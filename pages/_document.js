199f5fccf7d023bc422eaf0116372f1f4de7e14a
Anshuman Bhardwaj <anshu5074@gmail.com>
2024-11-01 18:04:21 +0530

    remove umami

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang={"en"}>
         <Head />
-        <script
-          async
-          defer
-          data-website-id="eca4e934-00c5-484c-8b75-6c6958e547d8"
-          src="https://umami.theanshuman.dev/umami.js"
-        ></script>
         <body>
           <Main />
           <NextScript />
